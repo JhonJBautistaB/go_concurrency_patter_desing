520826399da8f727dbe95fd6365c0ebf3d11accf
JhonJBautitaB <developer@SOFT-DEV-PY>
2023-08-07 18:26:48 -0500

    aprendiendo de sync.mutex bloqueando variabes que utilizan varias gorutines para no causar un race conditions

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+var (
+	balance int = 100
+)
+
+func Deposit(amount int, wg *sync.WaitGroup, lock *sync.Mutex) {
+	defer wg.Done()
+	lock.Lock() //bloquea una variable para que no sea utilizada por varias gorutinas
+	b := balance
+	balance = b + amount
+	lock.Unlock() //desploqueo
+}
+
+func Balance() int {
+	b := balance
+	return b
+}
+
+func main() {
+	var wg sync.WaitGroup
+	var lock sync.Mutex
+
+	for i := 1; i <= 5; i++ {
+		wg.Add(1)
+		go Deposit(i*100, &wg, &lock)
+	}
+	wg.Wait()
+	fmt.Println(Balance())
+
+}
