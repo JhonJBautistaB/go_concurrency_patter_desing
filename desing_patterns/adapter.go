f48eabc81e5e11f88dc089338099fb8ebd02a8c5
JhonJairoBautistaB <jhon.bautista@outlook.com>
2023-08-20 20:22:24 -0500

    implementando el patron de diseño Adapter, usando como ejemplo pagos, donde una clase no cumplia la implementación de la interfaz

---
+++ desing_patterns/adapter.go
@@ -0,0 +1,44 @@
+package main
+
+import "fmt"
+
+type Payment interface {
+	Pay()
+}
+
+type CashPayment struct{}
+
+func (CashPayment) Pay() {
+	fmt.Println("Paymen using Cash")
+}
+
+func ProccesPayment(p Payment) {
+	p.Pay()
+}
+
+type BankPayment struct{}
+
+func (BankPayment) Pay(bankAccount int) {
+	fmt.Printf("Paying using bankAccount %d\n", bankAccount)
+}
+
+type BankPaymentAdapter struct {
+	BankPayment *BankPayment
+	bankAccount int
+}
+
+func (bpa *BankPaymentAdapter) Pay() {
+	bpa.BankPayment.Pay(bpa.bankAccount)
+}
+
+func main() {
+	cash := &CashPayment{}
+	ProccesPayment(cash)
+	/* bank := &BankPayment{}
+	ProccesPayment(bank) */
+	bpa := &BankPaymentAdapter{
+		bankAccount: 5,
+		BankPayment: &BankPayment{},
+	}
+	ProccesPayment(bpa)
+}
